8fb0112a7808bc8f7a58da0fb176addfb197e714
Sang Saephan <sangsaephan510@yahoo.com>
2017-01-31 17:40:05 -0800

    Parse csv file

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -12,26 +12,49 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
 
     @IBOutlet weak var collectionView: UICollectionView!
     
+    var pokemon = [Pokemon]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.delegate = self
         collectionView.dataSource = self
+        
+        parsePokemon()
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 50
+        return pokemon.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as! PokeCell
-        let pokemon = Pokemon(name: "Pokemon", pokedexId: indexPath.row)
-        cell.configureCell(pokemon: pokemon)
+        let pokemon = self.pokemon[indexPath.row]
+        cell.configureCell(pokemon)
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
+    }
+    
+    func parsePokemon() {
+        let path = Bundle.main.path(forResource: "pokemon", ofType: "csv")
+        
+        do {
+            let csv = try CSV(contentsOfURL: path!)
+            let rows = csv.rows
+            
+            for row in rows {
+                let name = row["identifier"]!
+                let pokemonId = Int(row["id"]!)!
+                
+                let pokemon = Pokemon(name: name, pokedexId: pokemonId)
+                self.pokemon.append(pokemon)
+            }
+        } catch let error as NSError {
+            print(error.debugDescription)
+        }
     }
 
 }

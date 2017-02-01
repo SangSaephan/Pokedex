d484c156b34048648b593cc70b4df29575495cff
Sang Saephan <sangsaephan510@yahoo.com>
2017-01-31 16:56:46 -0800

    Conform to collection view protocols and display some data

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -8,14 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as! PokeCell
+        let pokemon = Pokemon(name: "Pokemon", pokedexId: indexPath.row)
+        cell.configureCell(pokemon: pokemon)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
 
 }
 
